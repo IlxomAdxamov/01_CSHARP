57b60e983ed05933fc09ff39a8bbf87004766422
ильхом <ilxomskru9@gmail.com>
2024-02-13 18:46:23 +0500

    Lesson4:Task3

---
+++ Lesson4/Task3/Program.cs
@@ -0,0 +1,16 @@
+﻿string CetLettersFromString(string s)
+{
+    string letters = "";
+    foreach (char e in s)
+    {
+        if (char.IsAsciiLetter(e) == true)
+        {
+            letters = letters + e;
+        }
+    }
+    return letters;
+}
+
+string str = Console.ReadLine();
+string result = CetLettersFromString(str);
+Console.WriteLine(result);
